b6cf97f393e14917237aa64b586c25c7b29884fc
lucascaovilla <lucasgrisac@gmail.com>
2023-03-27 19:54:08 -0300

    Reorganizing file managers
    Removed folder_man and zip_man.
    Changed mp3_man to mp3_files_man, wich will manage all files given by the user.

--- Flashcards - v1/zip_man.py
+++
@@ -1,9 +0,0 @@
-from zipfile import ZipFile
-
-file = "files\ING_U01_dialogo_-_Audios_frases.zip" #file path
-
-with ZipFile(file, 'r') as zip:
-    zip.printdir()
-    print('Processing...')
-    zip.extractall()
-    print('Process completed!')
