fd4c3adf795b567029c825c3c4c339367bf337bd
lucascaovilla <lucasgrisac@gmail.com>
2023-06-01 11:30:41 -0300

    Test version

---
+++ main.py
@@ -0,0 +1,4 @@
+#Anki application
+#receives the path of a folder with .zip mp3 archives
+#unzip the archives and add it to an Anki library as new flashcards
+#the flashcard text must be the mp3 file name
