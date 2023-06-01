7f896c689091cfafb2f63e34064d4aeb37f99941
lucascaovilla <lucasgrisac@gmail.com>
2023-06-01 11:29:35 -0300

    Initial commit

--- flashcards - v1/main.py
+++
@@ -1,4 +0,0 @@
-#Anki application
-#receives the path of a folder with .zip mp3 archives
-#unzip the archives and add it to an Anki library as new flashcards
-#the flashcard text must be the mp3 file name
